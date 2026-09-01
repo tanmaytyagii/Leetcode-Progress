b5fd40e1cdcebb544b7afb9b8638fb254c45d21b
Tanmay Tyagi <tanmaytyagi68@gmail.com>
2026-09-01 22:16:18 +0530

    Time: 1 ms (17.8%), Space: 25.1 MB (5.53%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> temp;
-        for(int i=0; i<n; i++){
-            if(nums[i] != 0){
-                temp.push_back(nums[i]);
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                ans.push_back(nums[i]);
             }
         }
-        int m = temp.size();
-        for (int i =0; i<m; i++){
-
-            nums[i]=temp[i];
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                ans.push_back(nums[i]);
+            }
         }
-        for(int i=m; i<n; i++){
-            nums[i]=0;
-        }
+        nums=ans;
         return;
     }
 };
